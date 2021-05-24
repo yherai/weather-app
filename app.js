2dbd636bbb1a01462aa0955ca5ec48081187c4c6
Jorge Garcia <jagu.ittis@gmail.com>
2021-05-24 19:08:48 +0200

    Console out the weather description, the temperature and the apparent temperature in farenheit scale

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const request = require('postman-request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=1fd03bca277378d586ad6a69f1401fd9&query=Pamplona';
+  'http://api.weatherstack.com/current?access_key=1fd03bca277378d586ad6a69f1401fd9&query=Pamplona&units=f';
 
-request(url, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request(url, { json: true }, (error, response) => {
+  const { current = {} } = response.body;
+  console.log(
+    `${current.weather_descriptions[0]}. It is currently ${current.temperature} degress out. It feels like ${current.feelslike} degress out`
+  );
 });
